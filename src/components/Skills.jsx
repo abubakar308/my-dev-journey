13a62498f1f30b84269c23e09c3c2e28d52d8c17
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-07-31 22:02:45 +0600

    skilles section added

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div>
+        <h2 className="text-2xl font-semibold mb-2">Tech Stack</h2>
+<ul className="list-disc ml-5">
+  <li><strong>Frontend:</strong> HTML, CSS, Tailwind CSS, DaisyUI, JavaScript, React</li>
+  <li><strong>Backend:</strong> Node.js, Express.js, MongoDB</li>
+  <li><strong>Tools:</strong> Git, GitHub, Firebase, Vercel, Netlify, Chrome DevTools</li>
+  <li><strong>Others:</strong> Figma, Postman, VS Code</li>
+</ul>
+
+    </div>
+  )
+}
